b8ee531c5132c97091dc69e90886d932d2a12f61
Skibin Alexander <skibinalexander@gmail.com>
2023-08-02 12:40:03 +0300

    Add git submodule for tangem-blst

--- build_blst.sh
+++ build_blst.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
-./blst/build.sh 'CC="xcrun -sdk iphoneos cc -arch arm64"'
+./blst-tangem/build.sh 'CC="xcrun -sdk iphoneos cc -arch arm64"'
 cp ./libblst.a ./Libraries/lib/iphoneos/
 rm -rf libblst.a
 
-./blst/build.sh 'CC="xcrun -sdk iphonesimulator cc -arch arm64"'
+./blst-tangem/build.sh 'CC="xcrun -sdk iphonesimulator cc -arch arm64"'
 cp ./libblst.a ./Libraries/lib/iphone-simulator/arm64/
 rm -rf libblst.a
 
-./blst/build.sh 'CC="xcrun -sdk iphonesimulator cc -arch x86_64"'
+./blst-tangem/build.sh 'CC="xcrun -sdk iphonesimulator cc -arch x86_64"'
 cp ./libblst.a ./Libraries/lib/iphone-simulator/x86_64/
 rm -rf libblst.a
